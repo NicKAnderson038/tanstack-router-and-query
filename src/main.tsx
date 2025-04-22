a2c5b7c6194f050ec58c79ef2df56ea8d160f76d
NicKAnderson038 <nick7anderson@protonmail.com>
2025-04-21 22:24:40 -0700

    rm useNav

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,6 @@
 import { StrictMode } from "react"
 import ReactDOM from "react-dom/client"
-import {
-  RouterProvider,
-  createRouter,
-  useNavigate,
-} from "@tanstack/react-router"
+import { RouterProvider, createRouter } from "@tanstack/react-router"
 
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen.ts"
@@ -25,13 +21,13 @@ const router = createRouter({
   scrollRestoration: true,
   defaultStructuralSharing: true,
   defaultNotFoundComponent: () => {
-    const navigate = useNavigate()
-    // gh-pages auto-nav
-    if (location.pathname.includes(pgJson.name)) {
-      navigate({
-        to: "/",
-      })
-    }
+    // const navigate = useNavigate()
+    // // gh-pages auto-nav
+    // if (location.pathname.includes(pgJson.name)) {
+    //   navigate({
+    //     to: "/",
+    //   })
+    // }
     return (
       <div>
         <p className="dark:text-white mb-2">Not Found</p>
